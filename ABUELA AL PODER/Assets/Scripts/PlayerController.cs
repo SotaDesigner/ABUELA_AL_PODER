e2f00552b09b4789d25f9db7e3fd61ec194bbef6
Toño <antoniogimenoo@hotmail.com>
2020-02-01 11:01:55 +0100

    Movimiento player

--- ABUELA AL PODER/Assets/Scripts/PlayerController.cs
+++ ABUELA AL PODER/Assets/Scripts/PlayerController.cs
@@ -5,22 +5,40 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D _rb;
-    float ejeX;
+    float _ejeX;
 
+    //Velocidad de movimiento del players
     public float velocidadMovimiento = 5;
-
-    Animator _anim;
+    
+    //Rotar al personaje si mira a la derecha o a la izquierda
+    bool miraDerecha = true;
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ejeX = Input.GetAxis("Horizontal");
+        //Me registra las teclas para el movimiento
+        _ejeX = Input.GetAxis("Horizontal");
+        _rb.velocity = new Vector2(_ejeX * velocidadMovimiento, _rb.velocity.y);
+    }
+    
+    void GestionAnimaciones()
+    {
+
+        if(_ejeX<0 && miraDerecha)
+        {
+            transform.Rotate(0, 180, 0);
+            miraDerecha = false;
+
+        } else if(_ejeX>0 && !miraDerecha)
+        {
+            transform.Rotate(0, 180, 0);
+            miraDerecha = true;
+        }
     }
 }
