e9085172119c46fe3a6bc5332ab35d97d03f6801
Toño <antoniogimenoo@hotmail.com>
2020-02-01 11:23:56 +0100

    objetos

--- ABUELA AL PODER/Assets/Scripts/Objeto.cs
+++ ABUELA AL PODER/Assets/Scripts/Objeto.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class Objeto : MonoBehaviour
 {
-    
+    bool _playerCerca = false;
+    bool _manoOcupada = false;
+    bool _posibleInteractuar = false;
+
+    Transform _player;
+    Transform _mano;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _mano = GameObject.Find("ManoDerecha").transform;
+        _player = GameObject.Find("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_playerCerca && !_manoOcupada && Input.GetKeyDown(KeyCode.E))
+        {
+            CogerObjeto();
+        }
+        else if(_manoOcupada&&Input.GetKeyDown(KeyCode.E))
+        {
+            SoltarObjeto();
+        }
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Interacturar();
+        }
     }
 }
